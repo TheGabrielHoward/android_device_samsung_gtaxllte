f517fb094afdbd02b33673d95bf74b8e12122e1a
followmsi <followmsi@gmail.com>
2019-01-14 23:15:51 +0100

    gtaxl: Fix SafetyNet (64-Bit)
    Samsung stock fingerprint are not working (anymore).
    Let's use Xiaomi Sagit fingerprint now.
    
    Nougat (still working fine on 32-Bit builds)
    samsung/gtaxlwifixx/gtaxlwifi:7.0/NRD90M/T580XXU2BQI9:user/release-keys
    
    Oreo (not working):
    samsung/gtaxlltexx/gtaxllte:8.1.0/M1AJQ/T585XXU4CRK5:user/release-keys

--- lineage_gtaxllte.mk
+++ lineage_gtaxllte.mk
@@ -32,8 +32,4 @@ PRODUCT_MODEL := SM-T585
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=gtaxlltexx \
-    PRIVATE_BUILD_DESC="gtaxlltexx-user 7.0 NRD90M T585XXU2BQI9 release-keys"
-
-BUILD_FINGERPRINT := samsung/gtaxlltexx/gtaxllte:7.0/NRD90M/T585XXU2BQI9:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/sagit/sagit:7.1.1/NMF26X/V8.2.17.0.NCACNEC:user/release-keys
