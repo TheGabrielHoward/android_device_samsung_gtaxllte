1eba89960aa2ab539bb0bddcb8f9ccab0de68ae3
followmsi <followmsi@gmail.com>
2019-11-28 09:36:35 +0100

    gtaxl: build new hidl based lights hal

--- device.mk
+++ device.mk
@@ -303,8 +303,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    lights.universal7870
+    android.hardware.light@2.0-service.samsung
 
 # Sensors
 PRODUCT_PACKAGES += \
